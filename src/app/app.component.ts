61766e71d0c7ed77281e51914d9718909f2aa43e
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-03-25 16:01:25 +0100

    slice pipe. with text, array and Ngfor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-name = 'Ramon';
+  name = 'Ramon';
+
+  array = [1,2,3,4,5,6,7,8,9,10];
 }
